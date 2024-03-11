7f69f87138e4ed9ce2f7ac901f0cd71b6df2b40e
Vinayak Chittora <vinayakchittora@gmail.com>
2024-03-11 23:11:36 +0530

    indentation changed auth.js

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,25 +1,21 @@
 import express from "express";
-const { PrismaClient } = require('@prisma/client')
+const { PrismaClient } = require("@prisma/client");
 
-const prisma = new PrismaClient()
-
+const prisma = new PrismaClient();
 
 export const authrouter = express.Router();
 
-authrouter.post("/signin", async (req:any, res:any)=>{
-
-	try{
-		const newUsers = await prisma.user.create({
-			data: {
-			 email: req.body.email,
-			 name: req.body.name,	
-			}
-		})
-		console.log("users creation success");
-		res.send(newUsers);
-	}catch(err){
-		console.error(err);
-	}
-
-	
+authrouter.post("/signin", async (req: any, res: any) => {
+  try {
+    const newUsers = await prisma.user.create({
+      data: {
+        email: req.body.email,
+        name: req.body.name,
+      },
+    });
+    console.log("users creation success");
+    res.send(newUsers);
+  } catch (err) {
+    console.error(err);
+  }
 });
