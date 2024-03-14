6b1b495b2efdcb3f205b0796777e0809abaa9b26
Gaurav Tak <gauravtak996@gmail.com>
2024-03-15 04:55:06 +0530

    added create new distributor route

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { User } from "../models/User.js";
+import { Dist } from "../models/Distributor.js";
 
 export const authrouter = express.Router();
 
@@ -11,9 +12,26 @@ authrouter.post("/userlogin", async (req, res) => {
       name
     });
     newUser.save();
+    res.json(newUser);
     console.log("user created success");
   } catch (err) {
     console.error(err);
     console.log("Error creating new user in DB");
+  }
+});
+
+authrouter.post("/distlogin", async (req, res) => {
+  const {email, name} = req.body;
+  try {
+    const newDist = new Dist({
+      email,
+      name
+    });
+    newDist.save();
+    res.json(newDist);
+    console.log("new distributor created successfully");
+  } catch (err) {
+    console.error(err);
+    console.log("Error creating new distributor in DB");
   }
 });
